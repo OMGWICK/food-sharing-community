ca01c36bcd38fabe6195352c624ca3be959cdd0a
wangyan <wang1136712124@163.com>
2021-02-06 11:16:47 +0800

    404page

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,7 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'Home',
     component: Home
@@ -17,9 +16,19 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
+      return import( /* webpackChunkName: "about" */ '../views/About.vue')
     }
-  }
+  },
+
+  {
+    path: '/404',
+    component: () => import('@/views/404'),
+  },
+
+  {
+    path: '*',
+    redirect: '/404',
+  },
 ]
 
 const router = new VueRouter({
